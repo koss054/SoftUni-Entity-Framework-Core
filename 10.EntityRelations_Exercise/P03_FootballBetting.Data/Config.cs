2b73b3f66876bf9b8552b81267190cd40bf4ea7a
koss054 <konstantin054@abv.bg>
2022-07-09 20:55:31 +0300

    Delete: Incorrect Naming

--- 10.EntityRelations_Exercise/P03_FootballBetting.Data/Config.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace P03_FootballBetting.Data
-{
-    public static class Config
-    {
-        public const string ConnectionString
-            = @"Server=localhost;Database=Bet366;Integrated Security=True";
-    }
-}
