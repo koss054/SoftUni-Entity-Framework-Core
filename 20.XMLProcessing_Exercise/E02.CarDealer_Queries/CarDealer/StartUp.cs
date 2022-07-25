afb7aed1305720a593f9098d567bf8d2ac338912
koss054 <konstantin054@abv.bg>
2022-07-25 20:59:36 +0300

    Update: XML Processing - Exercise
    Uploading the other solutions for this exercise.

--- 20.XMLProcessing_Exercise/E02.CarDealer_Queries/CarDealer/StartUp.cs
+++ 20.XMLProcessing_Exercise/E02.CarDealer_Queries/CarDealer/StartUp.cs
@@ -4,9 +4,11 @@
     using System.Collections.Generic;
     using System.IO;
     using System.Linq;
+    using System.Text;
     using System.Xml.Serialization;
 
     using CarDealer.Data;
+    using CarDealer.Dtos.Export;
     using CarDealer.Dtos.Import;
     using CarDealer.Models;
 
@@ -23,11 +25,14 @@
 
             //Console.WriteLine("Database reset successfully");
 
-            InitializeInputFilePath("sales.xml");
-            string xml = File.ReadAllText(filePath);
+            //InitializeInputFilePath("sales.xml");
+            //string xml = File.ReadAllText(filePath);
 
-            string result = ImportSales(dbContext, xml);
-            Console.WriteLine(result);
+            //string result = ImportSales(dbContext, xml);
+            //Console.WriteLine(result);
+
+            InitializeOutputtFilePath("sales-discounts.xml");
+            Console.WriteLine(GetSalesWithAppliedDiscount(dbContext));
         }
 
         //Problem 9 - Import Suppliers
@@ -187,9 +192,195 @@
             return $"Successfully imported {sales.Count}";
         }
 
+        //Problem 14 - Export Cars with Distance
+        public static string GetCarsWithDistance(CarDealerContext context)
+        {
+            var sb = new StringBuilder();
+
+            ExportCarsWithDistanceDto[] carDtos = context
+                .Cars
+                .Where(c => c.TravelledDistance > 2000000)
+                .OrderBy(c => c.Make)
+                .ThenBy(c => c.Model)
+                .Take(10)
+                .Select(c => new ExportCarsWithDistanceDto()
+                {
+                    Make = c.Make,
+                    Model = c.Model,
+                    TravelledDistance = c.TravelledDistance
+                })
+                .ToArray();
+
+            XmlSerializerNamespaces namespaces = new XmlSerializerNamespaces();
+            namespaces.Add(string.Empty, string.Empty);
+
+            XmlRootAttribute xmlRoot = new XmlRootAttribute("cars");
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(ExportCarsWithDistanceDto[]), xmlRoot);
+
+            using StringWriter writer = new StringWriter(sb);
+            xmlSerializer.Serialize(writer, carDtos, namespaces);
+
+            return sb.ToString().TrimEnd();
+        }
+
+        //Problem 15 - Export Cars from Make BMW
+        public static string GetCarsFromMakeBmw(CarDealerContext context)
+        {
+            var sb = new StringBuilder();
+
+            ExportBMWCarsDto[] carsDtos = context
+                .Cars
+                .Where(c => c.Make == "BMW")
+                .OrderBy(c => c.Model)
+                .ThenByDescending(c => c.TravelledDistance)
+                .Select(c => new ExportBMWCarsDto()
+                {
+                    Id = c.Id,
+                    Model = c.Model,
+                    TravelledDistance = c.TravelledDistance
+                })
+                .ToArray();
+
+            XmlSerializerNamespaces namespaces = new XmlSerializerNamespaces();
+            namespaces.Add(string.Empty, string.Empty);
+
+            XmlRootAttribute xmlRoot = new XmlRootAttribute("cars");
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(ExportBMWCarsDto[]), xmlRoot);
+
+            using StringWriter writer = new StringWriter(sb);
+            xmlSerializer.Serialize(writer, carsDtos, namespaces);
+
+            return sb.ToString().TrimEnd();
+        }
+
+        //Problem 16 - Export Local Suppliers
+        public static string GetLocalSuppliers(CarDealerContext context)
+        {
+            var sb = new StringBuilder();
+
+            ExportLocalSuppliersDto[] supplierDtos = context
+                .Suppliers
+                .Where(s => !s.IsImporter)
+                .Select(s => new ExportLocalSuppliersDto()
+                {
+                    Id = s.Id,
+                    Name = s.Name,
+                    PartsCount = s.Parts.Count
+                })
+                .ToArray();
+
+            XmlSerializerNamespaces namespaces = new XmlSerializerNamespaces();
+            namespaces.Add(string.Empty, string.Empty);
+
+            XmlRootAttribute xmlRoot = new XmlRootAttribute("suppliers");
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(ExportLocalSuppliersDto[]), xmlRoot);
+
+            using StringWriter writer = new StringWriter(sb);
+            xmlSerializer.Serialize(writer, supplierDtos, namespaces);
+
+            return sb.ToString().TrimEnd();
+        }
+
+        //Problem 17 - Export Cars with Their List of Parts
+        public static string GetCarsWithTheirListOfParts(CarDealerContext context)
+        {
+            var sb = new StringBuilder();
+
+            ExportCarsWithListOfPartsDto[] carDtos = context
+                .Cars
+                .Select(c => new ExportCarsWithListOfPartsDto()
+                {
+                    Make = c.Make,
+                    Model = c.Model,
+                    TravelledDistance = c.TravelledDistance,
+                    Parts = c.PartCars
+                        .Select(cp => new ExportCarPartsDto()
+                        {
+                            Name = cp.Part.Name,
+                            Price = cp.Part.Price
+                        })
+                        .OrderByDescending(cp => cp.Price)
+                        .ToArray()
+                })
+                .OrderByDescending(c => c.TravelledDistance)
+                .ThenBy(c => c.Model)
+                .Take(5)
+                .ToArray();
+
+            return Serialize(carDtos, "cars");
+        }
+
+        //Problem 18 - Export Total Sales by Customer
+        public static string GetTotalSalesByCustomer(CarDealerContext context)
+        {
+            ExportCustomerSalesDto[] customerDtos = context
+                .Customers
+                .Select(c => new ExportCustomerSalesDto()
+                {
+                    FullName = c.Name,
+                    BoughtCars = c.Sales.Count,
+                    SpentMoney = c.Sales
+                        .Select(s => s.Car)
+                            .SelectMany(c => c.PartCars)
+                            .Sum(pc => pc.Part.Price)
+                })
+                .Where(c => c.BoughtCars > 0)
+                .OrderByDescending(c => c.SpentMoney)
+                .ToArray();
+
+            return Serialize(customerDtos, "customers");
+        }
+
+        //Problem 19 - Export Sales with Applied Discount
+        public static string GetSalesWithAppliedDiscount(CarDealerContext context)
+        {
+            ExportSalesWithDiscountDto[] saleDtos = context
+                .Sales
+                .Select(s => new ExportSalesWithDiscountDto()
+                {
+                    Car = new ExportCarDto()
+                    {
+                        Make = s.Car.Make,
+                        Model = s.Car.Model,
+                        TravelledDistance = s.Car.TravelledDistance
+                    },
+                    Discount = s.Discount,
+                    CustomerName = s.Customer.Name,
+                    Price = s.Car.PartCars
+                        .Sum(pc => pc.Part.Price),
+                    PriceWithDiscount 
+                    = (s.Car.PartCars.Sum(pc => pc.Part.Price)) 
+                    - ((s.Car.PartCars.Sum(pc => pc.Part.Price)) * (s.Discount / 100))
+                })
+                .ToArray();
+
+            return Serialize(saleDtos, "sales");
+        }
+
         private static void InitializeInputFilePath(string fileName)
         {
             filePath = Path.Combine(Directory.GetCurrentDirectory(), "../../../Datasets/", fileName);
+        }
+
+        private static void InitializeOutputtFilePath(string fileName)
+        {
+            filePath = Path.Combine(Directory.GetCurrentDirectory(), "../../../Results/", fileName);
+        }
+
+        private static string Serialize<T>(T dto, string rootName)
+        {
+            var sb = new StringBuilder();
+
+            XmlSerializerNamespaces namespaces = new XmlSerializerNamespaces();
+            namespaces.Add(string.Empty, string.Empty);
+
+            XmlRootAttribute xmlRoot = new XmlRootAttribute(rootName);
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T), xmlRoot);
+
+            using StringWriter writer = new StringWriter(sb);
+            xmlSerializer.Serialize(writer, dto, namespaces);
+
+            return sb.ToString().TrimEnd();
         }
     }
 }
