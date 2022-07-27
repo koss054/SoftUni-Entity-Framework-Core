4388f38a2bcdafa655671aa4c9e796dc3b91cef4
koss054 <konstantin054@abv.bg>
2022-07-27 12:50:51 +0300

    Update: XML Processing - Exercise
    Updated the files with the rest of the solutions.

--- 20.XMLProcessing_Exercise/E01.ProductShop_Queries/ProductShop/ProductShopProfile.cs
+++ 20.XMLProcessing_Exercise/E01.ProductShop_Queries/ProductShop/ProductShopProfile.cs
@@ -1,11 +1,17 @@
-﻿using AutoMapper;
+﻿
 
 namespace ProductShop
 {
+    using ProductShop.Dtos.Product;
+    using ProductShop.Models;
+
+    using AutoMapper;
+
     public class ProductShopProfile : Profile
     {
         public ProductShopProfile()
         {
+
         }
     }
 }
